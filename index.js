f05091af221deff6775d5ccc6d7aecfcdb467fc4
UnaLai <qqq323618@gmail.com>
2019-09-06 13:47:46 +0800

    [feat]:add file routes.js

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 const http = require('http');
+const routes = require('./routes')
 
-const server = http.createServer((req, res) => {
-  let reqUrl = req.url
-  let reqMethod = req.method
-  if (reqUrl === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html lang="zh-cn">')
-    res.write('<head><title>node test</title></head>')
-    res.write('<body><form method="POST" action="/create-user"><input type="text" name="name"/><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    res.end()
-  }
-  if (reqUrl === '/users') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>node test</title></head>')
-    res.write('<body><ul><li>one</li><li>two</li><li>three</li></ul></body>')
-    res.write('</html>')
-    res.end()
-  }
-  if (reqUrl === '/create-user' && reqMethod === 'POST') {
-    const body =[]
-    req.on('data',(chunk)=>{
-      body.push(chunk)
-    })
-    req.on('end',()=>{
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody)
-    })
-    res.statusCode = 302;
-    res.setHeader('Location','/')
-    res.end()
-  }
-})
+const server = http.createServer(routes)
 
 server.listen(3000)
